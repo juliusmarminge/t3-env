35752f8c3f0cb17ce5d9eb1dce53ebca75e739f9
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-16 18:36:51 +0100

    tscheck

--- src/env.mjs
+++ src/env.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { z } from "zod";
 
 /**
@@ -6,7 +7,17 @@ import { z } from "zod";
  */
 export const server = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url()
+  ),
 });
 
 /**
@@ -26,6 +37,7 @@ export const client = z.object({
 export const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
@@ -56,5 +68,6 @@ if (parsed.success === false) {
   throw new Error("Invalid environment variables");
 }
 
-/** @type z.infer<merged> */
+/** @type z.infer<merged>
+ * @ts-ignore - technically lying here */
 export const env = parsed.data;
